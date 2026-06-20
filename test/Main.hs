dc12d5bf396cf8890763a9b244361bbe1b72896e
Gabriel-Emannuel <gabriel.emannuel.gama.andrade@gmail.com>
2026-06-20 15:52:56 +0000

    test: prepare environment for testing

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,17 @@
 module Main (main) where
 
+import MyLib (someFunc)
+
+import Test.HUnit
+
+testBase :: Test
+testBase = TestCase (assertEqual "Some Func" "Função" (someFunc))
+
+allTests :: Test
+allTests = TestList [TestLabel "Test Base" testBase]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  putStrLn "Running tests..."
+  runTestTT allTests
+  return ()
