71d9e7178fd5cc6315d1e8b6ea2998925438af14
Gabriel-Emannuel <gabriel.emannuel.gama.andrade@gmail.com>
2026-06-21 00:50:03 +0000

    feat: diversity activation functions

--- src/ActivationFunctions.hs
+++ src/ActivationFunctions.hs
@@ -8,6 +8,8 @@ type ThresholdValues = [ThresholdValue]
 type Value = Double
 type Values = [Value]
 
+type Alpha = Double
+
 threshold :: ThresholdValues -> Values -> ActivationFunction
 threshold thresholds inputs = (activationF, derivateF)
   where
@@ -17,3 +19,33 @@ threshold thresholds inputs = (activationF, derivateF)
         | v > maxValue = lastValue
         | otherwise = (snd . head . filter ((v>) . fst)) valuePairs
     derivateF _ = 0
+
+relu :: ActivationFunction
+relu = (f, f') 
+    where
+        f x = max 0.0 x
+        f' x = if x > 0.0 then 1.0 else 0.0
+
+leakyRelu :: ActivationFunction
+leakyRelu = (f, f')
+    where
+        f x = max (0.1 * x) x
+        f' x = if x > 0 then 1 else 0.1
+
+elu :: Alpha -> ActivationFunction
+elu alpha = (f, f')
+    where
+        f  x = if x >= 0 then x else alpha * (exp x - 1)
+        f' x = if x >= 0 then 1 else f x + alpha
+
+tanhF :: ActivationFunction
+tanhF = (f, f')
+    where
+        f    = tanh 
+        f' x = 1 - (tanh x) ** 2
+
+sigmoid :: ActivationFunction
+sigmoid = (f, f')
+    where
+        f x  = 1 / (1 + exp (-x))
+        f' x = f x * (1 - f x)
